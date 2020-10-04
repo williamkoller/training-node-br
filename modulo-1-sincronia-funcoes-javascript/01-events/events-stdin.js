bb6a523efc26b315d53553ef187a9b464e326a1a
William Koller <wkoller25@gmail.com>
2020-10-04 18:58:43 -0300

    feat: add events and events stdin on click

---
+++ modulo-1-sincronia-funcoes-javascript/01-events/events-stdin.js
@@ -0,0 +1,26 @@
+const EventEmitter = require('events')
+
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function (click) {
+    console.log('um usuario clicou', click)
+})
+
+const stdin = process.openStdin()
+
+function main() {
+    return new Promise((function (resolve, reject) {
+        stdin.addListener('data', function (value) {
+            //console.log(`Você digitou: ${value.toString().trim()}`)
+            return resolve(value)
+        })
+
+    }))
+}
+main().then(function (resultado) {
+    console.log('resultado: ', resultado.toString())
+})
